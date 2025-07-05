5310ede03bdd35fcfa463a2a0c89ef5b67d67a88
mrkjhm <162660469+mrkjhm@users.noreply.github.com>
2025-07-05 08:26:34 +0800

    initial commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true})); 
 
 const corsOptions = {
-    origin: ['http://localhost:3000', 'https://fitlog-peach.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://fitlog-peach.vercel.app'],
     credentials: true,
     optionsSuccessStatus: 200
 }
